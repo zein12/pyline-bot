2e364c084c990ed0eb03d872661fc182a8ebe110
zein12 <jena.zein@gmail.com>
2017-02-24 01:32:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(reply_token, TextSendMessage(text='Hello World!'))
+    line_bot_api.reply_message(event.reply_token,
+TextSendMessage(text='Hello World!'))
     line_bot_api.multicast(['to1', 'to2'], TextSendMessage(text='Hello World!'))
 
 
