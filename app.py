52352341396673bfae41a0105d133a3804494375
zein12 <jena.zein@gmail.com>
2017-02-24 01:41:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,8 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-TextSendMessage(text=event.'naon'))
+TextSendMessage(text=event.message.text))
+    line_bot_api.push_message(to, TextSendMessage(text='Hello World!'))
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
